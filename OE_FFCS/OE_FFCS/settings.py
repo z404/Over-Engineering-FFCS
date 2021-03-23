49a8709301fea8d49603ebab08b7f6b7245647c0
sankalpmukim <sankalpmukim@gmail.com>
2021-03-23 16:00:18 +0530

    build: Setting up the app

--- OE_FFCS/OE_FFCS/settings.py
+++ OE_FFCS/OE_FFCS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oeffcs',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'OE_FFCS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- OE_FFCS/oeffcs/views.py
+++ OE_FFCS/oeffcs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def testView(request):
+    return HttpResponse("test view yayy")

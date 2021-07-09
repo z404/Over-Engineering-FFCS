f94a81207ad5a2f0aa30dbab84998c7f5f05b869
sankalpmukim <sankalpmukim@gmail.com>
2021-07-09 19:54:01 +0530

    Fix: loading screen initial update

--- OE_FFCS/oeffcs/static/js/timetablegenloading.js
+++ OE_FFCS/oeffcs/static/js/timetablegenloading.js
@@ -5,7 +5,7 @@ const func = async () => {
         'valid_status':false
     };
     const firstCall = async () => {
-        const firstres = await fetch("/loadingscreenstatus/", {
+        const res = await fetch("/loadingscreenstatus/", {
             method: "POST",
             body: JSON.stringify({}),
             headers: {
@@ -13,10 +13,20 @@ const func = async () => {
                 "X-CSRFToken": csrftoken,
             }
         });
-        const json = await firstres.json();
-        document.getElementById("info").innerText=`Working with ${ json['total_timetables'] } timetables here, so we appreciate your cooperation :)`
+        result = {
+            ...result,
+            ...(await res.json())
+        }
+        let percentage = Number(result['completed_timetables'])/Number(result['total_timetables'])*100;
+        percentage = percentage.toPrecision(3);
+        progress.style.width = percentage + '%';
+        progress.style.opacity = 1;
+        document.getElementById("progress-done").innerText= String(percentage) +"%";
+        document.getElementById("valid-timetables").innerText= String(result['valid_timetables']) + " valid found!";
     };
-    firstCall();
+    setTimeout(() => {
+        firstCall();
+    }, 1000);
     const progress = document.querySelector('.progress-done');
     const repeater = setInterval(async () => {
         const res = await fetch("/loadingscreenstatus/", {

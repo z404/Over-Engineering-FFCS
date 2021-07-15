1fa3efccef2c75eb5145d71334deb1f797b89884
sankalpmukim <sankalpmukim@gmail.com>
2021-07-15 20:03:24 +0530

    favicon addition

--- OE_FFCS/OE_FFCS/urls.py
+++ OE_FFCS/OE_FFCS/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('oeffcs.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,

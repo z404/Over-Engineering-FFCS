0126934f857ac662d6ac7dca6fb62d632474abb3
sankalpmukim <sankalpmukim@gmail.com>
2021-12-02 02:02:06 +0530

    Conflict data updates

--- OE_FFCS/oeffcs/static/js/genteachlist.js
+++ OE_FFCS/oeffcs/static/js/genteachlist.js
@@ -1,61 +1,59 @@
-const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]").value;
-
 const render_timetable = (tmtbl) => {
-    all_text = localStorage.getItem("timetable");
+  all_text = localStorage.getItem("timetable");
 
-    const conventional = (slot) => '<td class="normal">' + slot + "</td>";
+  const conventional = (slot) => '<td class="normal">' + slot + "</td>";
 
-    const activated = (slotinfo) =>
-        '<td class="normal active">' + slotinfo + "</td>";
+  const activated = (slotinfo) =>
+    '<td class="normal active">' + slotinfo + "</td>";
 
-    tmtbl.forEach((enrollment) => {
-        slots_in_enrollment = enrollment.split(" ")[0].split("+");
-        enrollment_data = enrollment.split(" ").slice(1).join(" ");
-        slots_in_enrollment.forEach((slot) => {
-            all_text = all_text.replaceAll(
-                conventional(slot),
-                activated(slot + "<br>" + enrollment_data)
-            );
-        });
+  tmtbl.forEach((enrollment) => {
+    slots_in_enrollment = enrollment.split(" ")[0].split("+");
+    enrollment_data = enrollment.split(" ").slice(1).join(" ");
+    slots_in_enrollment.forEach((slot) => {
+      all_text = all_text.replaceAll(
+        conventional(slot),
+        activated(slot + "<br>" + enrollment_data)
+      );
     });
-    return all_text;
+  });
+  return all_text;
 };
 
 function toggle_modal(modal) {
-    modal.style.display = "block";
+  modal.style.display = "block";
 
-    window.onclick = function(event) {
-        //Logic for close
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    span_close = document.getElementById(modal.id + "close");
-    span_close.onclick = function() {
-        modal.style.display = "none";
-    };
-    const TTID = event.target.dataset.ttid;
-    if (document.getElementById(TTID + "main").innerHTML == "") {
-        fetch("/modaldata/", {
-                method: "POST",
-                body: JSON.stringify({
-                    ttid: TTID,
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8",
-                    "X-CSRFToken": csrftoken,
-                },
-            })
-            .then((res) => res.json())
-            .then((res) => {
-                document.getElementById(TTID + "main").innerHTML = render_timetable(
-                    res["render_timetable"]
-                );
-                document.getElementById(TTID + "info").innerHTML =
-                    res["information_table"];
-            });
+  window.onclick = function (event) {
+    //Logic for close
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    // .catch(err => console.error(err));
+  };
+  span_close = document.getElementById(modal.id + "close");
+  span_close.onclick = function () {
+    modal.style.display = "none";
+  };
+  const TTID = event.target.dataset.ttid;
+  if (document.getElementById(TTID + "main").innerHTML == "") {
+    fetch("/modaldata/", {
+      method: "POST",
+      body: JSON.stringify({
+        ttid: TTID,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "X-CSRFToken": csrftoken,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        document.getElementById(TTID + "main").innerHTML = render_timetable(
+          res["render_timetable"]
+        );
+        document.getElementById(TTID + "info").innerHTML =
+          res["information_table"];
+      });
+  }
+  // .catch(err => console.error(err));
 }
 
 // // Get the modal
